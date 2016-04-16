2adfd73df34f43e7bbbfce1cd47dd598774ba4dc
pmac85 <pmac_85@hotmail.com>
2016-04-16 23:04:49 +0100

    Login e Logout adicionados e session controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,13 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
-  get 'rules' => 'static_pages#rules'
+  get    'rules'   => 'static_pages#rules'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
