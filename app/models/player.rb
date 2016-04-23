48bd9bf742fccf018856cb65ca0474b420aa1604
André Lara <a64362@alunos.uminho.pt>
2016-04-24 00:30:37 +0100

    Adicionei validação que não permite mais de 15 Players numa Team

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,16 @@ class Player < ActiveRecord::Base
   validates :name,      presence: true
   validates :position,  presence: true
   validates :value,     presence: true
+  validate :validate_number_in_team
+
+
+  private
+  def validate_number_in_team
+    if team_id && Player.where(team_id: team_id).size >= 15
+        errors.add(:team_id, "Error in team_id, team can't have more than 15 players")
+        return false
+    end
+
+    return true
+  end
 end
