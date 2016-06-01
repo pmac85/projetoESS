8a1e8d573b93996fe02964adefc190d9c4411dba
Rangedz <andrelara1111@hotmail.com>
2016-06-01 21:23:53 +0100

    Fix bug

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -11,14 +11,14 @@ class JourneysController < ApplicationController
     i = journey.id - 1
     previousj = Journey.find_by_id(i)
     if !journey.is_closed
-      if journey.id != 1 && previousj.is_closed
+      if journey.number == 1
         journey.games.each do |game|
           game.gerarResultado
         end
         journey.is_closed = true
         journey.save
         flash[:info] = "Matchday #{journey.number} closed."
-      elsif journey.id == 1
+      elsif previousj.is_closed
         journey.games.each do |game|
           game.gerarResultado
         end
